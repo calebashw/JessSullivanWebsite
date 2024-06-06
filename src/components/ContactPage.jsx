88cfa2c4b192b7559029c24d2aff1b3c6737a63a
Caleb Ash <124649246+calebashw@users.noreply.github.com>
2024-06-05 21:32:24 -0400

    feat: adding email and appointment icons + words

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -7,6 +7,18 @@ function ContactPage() {
     <div className="contact-page-container">
       <NavBar />
       <h1>contact page</h1>
+      <div className="contact-section">
+        <ul>
+          <li><i className="fas fa-envelope fa-2x" />
+            {/* <p>Email</p><br /> */}
+            jess@jessicasullivan.com
+          </li>
+          <li>
+            <i className="fas fa-calendar-alt fa-2x" />
+            Available by appointment only
+          </li>
+        </ul>
+      </div>
       <footer>
         <ul>
           <li><a href="https://www.facebook.com/groups/152660442061070/" aria-label="Facebook group" target="_blank" rel="noreferrer"><i className="fab fa-facebook" /></a></li>
